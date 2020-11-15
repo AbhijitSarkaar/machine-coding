88652beb5be7b06d1c37d4130c831135d1d1bad6
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-15 18:55:20 +0530

    Attach event handler on dynamically created element using event delegation

--- ToDo/index.js
+++ ToDo/index.js
@@ -1,13 +1,24 @@
 let inputText = '';
 
-function getItemHTML() {
+function ItemHTML() {
 	let item = document.createElement('div');
 	let itemText = document.createElement('div');
 	let deleteButton = document.createElement('div');
+
+	// deleteButton.addEventListener('click', () => {
+	// 	alert('item deleted');
+	// });
+
+	//set the ids
 	item.id = 'item';
 	itemText.id = 'item-text';
 	deleteButton.id = 'delete-icon';
+
+	//insert inner texts
 	itemText.innerText = inputText;
+	deleteButton.innerText = 'Delete';
+
+	//append the created elements
 	item.appendChild(itemText);
 	item.appendChild(deleteButton);
 	return item;
@@ -16,7 +27,8 @@ function getItemHTML() {
 document.getElementById('add-item').addEventListener('click', () => {
 	if (inputText.length) {
 		let toDoItems = document.getElementById('items-container');
-		toDoItems.appendChild(getItemHTML());
+		toDoItems.appendChild(ItemHTML());
+		//clearing the input
 		inputText = '';
 		document.getElementById('item-input').value = '';
 	} else {
@@ -24,8 +36,10 @@ document.getElementById('add-item').addEventListener('click', () => {
 	}
 });
 
-document.getElementById('delete-icon').addEventListener('click', () => {
-	alert('item deleted');
+document.addEventListener('click', (e) => {
+	if (e.target && e.target.id === 'delete-icon') {
+		alert('item deleted');
+	}
 });
 
 document.getElementById('item-input').addEventListener('input', (e) => {
