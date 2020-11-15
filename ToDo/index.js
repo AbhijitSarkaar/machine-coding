ce8c27da4c9b9d2ff98d759fd87f7f61a899270a
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-15 18:17:28 +0530

    Add empty text item

--- ToDo/index.js
+++ ToDo/index.js
@@ -1,5 +1,18 @@
+function getItemHTML() {
+	let item = document.createElement('div');
+	let itemText = document.createElement('div');
+	let deleteButton = document.createElement('div');
+	item.id = 'item';
+	itemText.id = 'item-text';
+	deleteButton.id = 'delete-icon';
+	item.appendChild(itemText);
+	item.appendChild(deleteButton);
+	return item;
+}
+
 document.getElementById('add-item').addEventListener('click', () => {
-	alert('item added');
+	let toDoItems = document.getElementById('items-container');
+	toDoItems.appendChild(getItemHTML());
 });
 
 document.getElementById('delete-icon').addEventListener('click', () => {
