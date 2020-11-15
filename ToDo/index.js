b45b1fda2a80656ce6b1f42736cb6d3ed7fde9d2
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-15 18:01:03 +0530

    Attach alert handlers on add and delete button

---
+++ ToDo/index.js
@@ -0,0 +1,7 @@
+document.getElementById('add-item').addEventListener('click', () => {
+	alert('item added');
+});
+
+document.getElementById('delete-icon').addEventListener('click', () => {
+	alert('item deleted');
+});
