147973d51598fc6046cab370ff28aea398dffc17
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-15 18:29:38 +0530

    Add input text

--- ToDo/index.js
+++ ToDo/index.js
@@ -1,3 +1,5 @@
+let inputText = '';
+
 function getItemHTML() {
 	let item = document.createElement('div');
 	let itemText = document.createElement('div');
@@ -5,16 +7,27 @@ function getItemHTML() {
 	item.id = 'item';
 	itemText.id = 'item-text';
 	deleteButton.id = 'delete-icon';
+	itemText.innerText = inputText;
 	item.appendChild(itemText);
 	item.appendChild(deleteButton);
 	return item;
 }
 
 document.getElementById('add-item').addEventListener('click', () => {
-	let toDoItems = document.getElementById('items-container');
-	toDoItems.appendChild(getItemHTML());
+	if (inputText.length) {
+		let toDoItems = document.getElementById('items-container');
+		toDoItems.appendChild(getItemHTML());
+		inputText = '';
+		document.getElementById('item-input').innerText = '';
+	} else {
+		alert('input is empty');
+	}
 });
 
 document.getElementById('delete-icon').addEventListener('click', () => {
 	alert('item deleted');
+});
+
+document.getElementById('item-input').addEventListener('input', (e) => {
+	inputText = e.target.value;
 });
