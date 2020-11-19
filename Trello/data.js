7e7e11d520affe4c15682683420a34490ac183be
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-11-19 17:44:47 +0530

    Mock data for todo and done status

---
+++ Trello/data.js
@@ -0,0 +1,14 @@
+export const todo = [
+	{
+		name: 'brush',
+		desc: 'to brush everyday',
+		status: 'todo',
+	},
+];
+export const done = [
+	{
+		name: 'coding',
+		desc: 'to code everyday',
+		status: 'done',
+	},
+];
