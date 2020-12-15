c1ba38655e91b6e18d6b3a089c3920cb85a76881
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-12-15 11:03:42 +0530

    reset timer

--- Timer/index.js
+++ Timer/index.js
@@ -1,6 +1,30 @@
+let totalMinutes = 1, totalSeconds = 0;
+let nextEvents = []
+
+getSeconds = () => {
+    for(let j = 1; j <= 10; ++j) {
+        nextEvents.push(setTimeout(() => {
+            document.getElementById('second-container').innerText = j;
+        }, j * 1000))
+    }
+}
+
+clearScheduledEvents = () => {
+    for(let i = 0; i < nextEvents.length; ++i) {
+        clearTimeout(nextEvents[i]);
+    }
+}
+
 document.getElementById('start-button').addEventListener('click', (event) => {
-    alert(event.target.id)
+    for(let i = 1; i <= totalMinutes; ++i) {
+        getSeconds();
+    }
 })
 document.getElementById('reset-button').addEventListener('click', (event) => {
-    alert(event.target.id)
+    document.getElementById('minute-container').innerText = 0;
+    document.getElementById('second-container').innerText = 0;
+
+    //need to clear the scheduled events using setTimeout
+    clearScheduledEvents();
+    nextEvents = [];
 })
---
+++ Timer/modules.js
@@ -0,0 +1,8 @@
+export function addTimer = () => {
+    // for(let i = 1; i <= 10; ++i) {
+    //     setTimeout(() => {
+    //         document.getElementById('minute-container').innerText = i;
+    //     }, i * 1000)
+    // }
+    alert('modules connected')
+}
