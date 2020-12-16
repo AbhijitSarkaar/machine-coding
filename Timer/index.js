b4c6f090ff0003111dd8d975e1bda67c8193885b
Abhijit Sarkar <abhijit.sarkar@embibe.com>
2020-12-16 17:17:19 +0530

    Built the increasing timer using setInterval

--- Timer/index.js
+++ Timer/index.js
@@ -1,43 +1,31 @@
-let totalMinutes = 2,
-	totalSeconds = 0;
-let currentMinute = 0;
+let hours = 0;
+let minutes = 0;
+let seconds = 0;
 let nextEvents = [];
-
-getSeconds = () => {
-	let promises = [];
-	for (let j = 10 * currentMinute + 1; j <= 10 * currentMinute + 10; ++j) {
-		let promise = new Promise((resolve, reject) => {
-			nextEvents.push(
-				setTimeout(() => {
-					if (j % 10 === 0) {
-						document.getElementById('minute-container').innerText = currentMinute + 1;
-					}
-					document.getElementById('second-container').innerText = j % 10;
-					resolve(j);
-				}, j * 1000)
-			);
-		});
-		promises.push(promise);
-	}
-	if (currentMinute === totalMinutes) {
-		Promise.all(promises).then((res) => {
-			setTimeout(() => {
-				alert('alert after all');
-			}, 0);
-		});
-	}
-};
 
 clearScheduledEvents = () => {
 	for (let i = 0; i < nextEvents.length; ++i) {
-		clearTimeout(nextEvents[i]);
+		clearInterval(nextEvents[i]);
 	}
 };
 
 document.getElementById('start-button').addEventListener('click', (event) => {
-	for (let i = 1; i <= totalMinutes; ++i) {
-		getSeconds();
-	}
+	nextEvents.push(
+		setInterval(() => {
+			seconds++;
+			if (seconds === 60) {
+				minutes++;
+				if (minutes === 60) {
+					hours++;
+					document.getElementById('hour-container').innerText = hours;
+					minutes = 0;
+				}
+				document.getElementById('minute-container').innerText = minutes;
+				seconds = 0;
+			}
+			document.getElementById('second-container').innerText = seconds;
+		}, 1000)
+	);
 });
 document.getElementById('reset-button').addEventListener('click', (event) => {
 	document.getElementById('hour-container').innerText = 0;
